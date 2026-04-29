fb52ff8c3dec15907a6bc75300821a9d9253d16c
lcjerrylu <lcjerrylu@tencent.com>
2026-04-29 12:09:34 +0800

    ci: add libxcb-cursor0 and other xcb libs for Qt 6.5+ on Ubuntu 24.04
    Qt 6.5+ 强制要求 libxcb-cursor0，Ubuntu 24.04 默认不装，导致
    QApplication 构造时 SIGABRT。一次性把 xcb 全家桶装齐。

--- hero_side_ui/__init__.py
+++ hero_side_ui/__init__.py
@@ -2,7 +2,7 @@
 HeroSideUI - PySide6 component library inspired by HeroUI v2
 """
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 from .components import Button, Accordion, AccordionItem, Input
 
